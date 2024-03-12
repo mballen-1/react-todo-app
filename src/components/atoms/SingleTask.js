ef3bfb1f07c49a6f785f92d851b0b415e77a22ac
Miguel Ángel Ballén Galindo <miguel.ballen@wizeline.com>
2024-03-12 15:16:18 -0500

    chore: update app.

--- src/components/atoms/SingleTask.js
+++ src/components/atoms/SingleTask.js
@@ -3,38 +3,65 @@ import { useState, useRef } from 'react';
 
 function SingleTask({ task, onRemoveTask, onTaskChange, onNameChange }) {
     const [editorMode, setEditorMode] = useState(false);
+    const [hovered, setHovered] = useState(false);
     const ref = useRef(null)
 
     const handleClick = e => {
         setEditorMode(true);
     }
 
+    const handleMouseEvent = (value) => {
+        setHovered(value);
+    }
+
+    const liStyles = {
+        textDecoration: task.completed ? 'line-through' : 'none',
+        display: 'flex',
+        justifyContent: 'space-between'
+    }
+
     if (task) {
         return (
             <li
                 key={task.id}
-                className={task.completed ? 'task_li__font-style' : ''}
                 ref={ref}
+                onMouseEnter={() => handleMouseEvent(true)}
+                onMouseLeave={() => handleMouseEvent(false)}
+                style={liStyles}
             >
                 <input
                     type="checkbox"
                     checked={task.completed}
-                    onChange={() => onTaskChange(task)} />
+                    onChange={() => onTaskChange(task)}
+                />
                 {
                     editorMode ?
-                        <input 
-                            type="text" 
-                            value={task.name} 
+                        <input
+                            type="text"
+                            value={task.name}
                             onChange={(e) => onNameChange(e, task)}
                             onBlur={() => setEditorMode(false)} autoFocus
                             name="taskNameEditor"
-                            />
+                        />
                         :
-                        <span onDoubleClick={handleClick}>
+                        <span onDoubleClick={handleClick} 
+                                style={{
+                                    width: '500px'
+                                }}>
                             {task.name}
                         </span>
                 }
-                <button onClick={() => onRemoveTask(task.id)}>x</button>
+                {
+                    hovered ?
+                        <button 
+                            onClick={() => onRemoveTask(task.id)}
+                            className='task_close_button__size'
+                        >
+                            x
+                        </button>
+                        :
+                        <div className='task_close_button__size'/>
+                }
             </li>
         )
     }
